df2de0feb05411436ec28963788f9c9d9fee7bf3
Dan Gorman <danthemangorman@gmail.com>
2023-12-05 16:31:40 -0700

    added time counter to template

--- template.py
+++ template.py
@@ -1,7 +1,14 @@
+import time
 
 def main():
     with open('input.txt') as f:
         lines = f.readlines()
 
+    return None
+
 if __name__ == '__main__':
-    main()
+    x = time.perf_counter_ns()
+    answer = main()
+    y = time.perf_counter_ns()
+    print('Answer:', answer)
+    print('Time (ms):', (y-x)/1000)
