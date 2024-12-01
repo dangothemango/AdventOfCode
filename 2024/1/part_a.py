6d6566a88cb0e034fd9c36392975f199323a06b2
Dan Gorman <danthemangorman@gmail.com>
2024-12-01 13:53:37 -0600

    reorganized, updated template, and 2024/1

--- 2024/1/part_a.py
+++ 2024/1/part_a.py
@@ -12,7 +12,18 @@ def get_data():
     return aocd.get_data()
 
 def main(data):
-    return None
+    A,B=list(),list()
+    #Parse list
+    for line in data.split('\n'):
+        nums=re.split(r'\s+', line)
+        A.append(int(nums[0]))
+        B.append(int(nums[1]))
+    A.sort()
+    B.sort()
+    diffSum=0
+    for i in range(len(A)):
+        diffSum+=abs(A[i]-B[i])
+    return diffSum
 
 if __name__ == '__main__':
     assert main(EXAMPLE_DATA) == EXAMPLE_ANSWER
--- 2024/1/part_b.py
+++ 2024/1/part_b.py
@@ -6,13 +6,26 @@ EXAMPLE_DATA='''3   4
 1   3
 3   9
 3   3'''
-EXAMPLE_ANSWER=11
+EXAMPLE_ANSWER=31
 
 def get_data():
     return aocd.get_data()
 
 def main(data):
-    return None
+    A,B=list(),dict()
+    #Parse list
+    for line in data.split('\n'):
+        nums=re.split(r'\s+', line)
+        A.append(int(nums[0]))
+        bNum=int(nums[1])
+        if bNum not in B:
+            B[bNum] = 0
+        B[bNum]+=1
+    A.sort()
+    simScore=0
+    for i in A:
+        simScore+=i * (B[i] if i in B else 0)
+    return simScore
 
 if __name__ == '__main__':
     assert main(EXAMPLE_DATA) == EXAMPLE_ANSWER
